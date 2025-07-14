78e8de979553afc65da9d6f37205c91661d22ec6
Nathan <heidtna@gmail.com>
2025-07-14 09:44:23 -0500

    Added ability to send a single prompt as a CLI argument. Prints response and token counts.

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from ai-agent!")
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+def main():
+    args = sys.argv
+    if len(args) == 1: 
+        print("No arguments given. Enter a prompt to get a response.")
+        exit(1)
+
+    client = genai.Client(api_key=api_key)
+    res = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=args[1])
+
+    print(res.text)
+    print(f"Prompt tokens: {res.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {res.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
