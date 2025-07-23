6cb664fab3b5fd6f4d834b4d4ab5da3888367ba4
Nathan <heidtna@gmail.com>
2025-07-22 20:43:30 -0500

    Updated 'run_python' file to 'run_python_file'. Added function declarations for remaining function files. Updated system prompt to allow LLM to access additional functions.

--- functions/declarations.py
+++ functions/declarations.py
@@ -15,8 +15,63 @@ schema_get_files_info = types.FunctionDeclaration(
     ),
 )
 
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Accepts a path to a file, then reads the content and returns a string of those contents up to a maximum character limit, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file from which to retrieve the contents."
+            ),
+        },
+    ),
+)
+
+schema_run_python_file = types.FunctionDeclaration(
+    name="run_python_file",
+    description="Takes a python file within 'working_directory' and executes the code inside. 'file_path' must be located within the working_directory to limit access to other files.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file from which to retrieve the contents."
+            ),
+            "args": types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(type=types.Type.STRING), # Need this to keep LLM from complaining about not being given params.
+                description="An array of optional string parameters. If not provided, default to None."
+            )
+        },
+        required=["file_path"], # This probably helps, too.
+    ),
+)
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description=" Writes contents to a file specified within the working directory. Overwrites the file if it already exists.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file from which to retrieve the contents."
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The text to be written to the file."
+            )
+        },
+    ),
+)
+
 available_functions = types.Tool(
     function_declarations = [
         schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_file,
+        schema_write_file
     ]
 )
--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
-from functions.run_python import run_python_file
+from functions.run_python_file import run_python_file
 
 
 class TestGetFileInfo(unittest.TestCase):
